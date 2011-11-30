21a7f4a8a5715dcbefeedc17287d1e38be2de8ae
Thaddee Tyl <thaddee.tyl@gmail.com>
2011-11-30 22:31:48 +0100

    Publishing script.
    Signed-off-by: Thaddee Tyl <thaddee.tyl@gmail.com>

---
+++ publi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash --norc
+
+# Which specs do we want to publish?
+specs=". dom4 es6 xhr2"
+
+# Thanks! I'll do that.
+
+git checkout gh-pages
+
+for spec in $specs
+do
+  cat Readme.md | Markdown.pl > "$spec/index.html"
+  git add "$spec/index.html"
+done
+
+git commit -am"I luv to publish stuff to github ❤ !"
+git push origin gh-pages
+
+git checkout master
+
